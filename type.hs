6e5a936107adabe0e1905c62880b46cc9cfb6091
beiciye <beiciye@gmail.com>
2016-07-23 22:28:40 +0800

    s

--- type.hs
+++ type.hs
@@ -3,5 +3,5 @@ surface :: Shape -> Float
 surface (Circle _ _ r) = pi *r^2
 surface (Rectangle x1 y1 x2 y2) = (abs $ x2 - x1) * (abs $ y2 - y1)
 
-addTwo :: (Num a) => a -> a -> a
-addTwo a b = a + b
+addTwo :: (Num a) => a -> a -> a -> a
+addTwo a b c = a + b + c
